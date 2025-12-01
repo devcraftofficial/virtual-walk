17be9e66de68673a26bcc72dacebba72bbdcdac8
devcraftofficial <mr.rishvan.a@gmail.com>
2025-12-01 17:20:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -199,4 +199,6 @@ def upload():
 # Start Server
 # --------------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
+
+
