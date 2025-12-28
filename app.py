40c45e6d6815343d4a3a305136ec038521021047
devcraftofficial <mr.rishvan.a@gmail.com>
2025-12-28 22:26:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -907,4 +907,5 @@ def like_street(street_id):
 # --------------------------------------------------------
 if __name__ == "__main__":
     app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
 
